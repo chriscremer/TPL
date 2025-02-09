b0db9cd963e166fff8b850dd581fac4a4c195d80
Chris <chris.a.cremer@gmail.com>
2025-02-08 23:01:17 -0500

    update

--- streamlit_site/my_pages/settings.py
+++ streamlit_site/my_pages/settings.py
@@ -70,7 +70,7 @@ def settings_page():
 
     if logout:
         st.session_state['login_status'] = False
-        print (f"Logging out {st.session_state['team_name']}")
+        # print (f"Logging out {st.session_state['team_name']}")
         # erase session state
         for key in list(st.session_state.keys()):
             del st.session_state[key]
